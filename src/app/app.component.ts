f35e6f60ab14694be1abb8ec4e075d490a21b20b
sathyapv92 <sathapv92@gmail.com>
2018-03-12 09:47:38 +0530

    *

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   selector: 'app-root',
+   template: `
+				<app-observable></app-observable>
+				<app-promise></app-promise>
+             `
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent { 
+
 }
+    
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 
-
-import { AppComponent } from './app.component';
-
+import { AppComponent }  from './app.component';
+import { ObservableComponent }  from './observable.component';
+import { PromiseComponent }  from './promise.component';
+import { BookService } from './book.service';
+import { BookData } from './book-data';
 
 @NgModule({
+  imports: [     
+        BrowserModule,
+		FormsModule,
+		HttpModule,
+		InMemoryWebApiModule.forRoot(BookData)
+  ],
   declarations: [
-    AppComponent
+        AppComponent,
+		ObservableComponent,
+		PromiseComponent
   ],
-  imports: [
-    BrowserModule
+  providers: [
+        BookService
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+        AppComponent
+  ]
 })
 export class AppModule { }
